3b4a88b4d581e61f3e7ece6be05468f33b7c50ba
Kang Noir <93761595+noircode@users.noreply.github.com>
2022-11-26 04:20:13 +0000

    add contact list with react components

--- contacts-app/src/index.js
+++ contacts-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import ContactApp from './ContactApp';
 
-const element = <h1>Hello, world!</h1>;
+// styling
+import './style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<ContactApp />);
